014612595b7fef1846b96609b6ad0f0c5226e692
Liangjun Zheng <zhengliangjun@outlook.com>
2017-12-12 09:54:15 +0800

    添加手机客户端项目

---
+++ Mobile/src/components/home/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View } from 'react-native';
+
+class HomeIndexView extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+HomeIndexView.propTypes = {
+
+};
+
+export default HomeIndexView;
---
+++ Mobile/src/components/user/login.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class UserLogin extends Component {
+    render() {
+        return (
+            <View>
+                
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container:{
+        
+    },
+});
+
+export default UserLogin;
